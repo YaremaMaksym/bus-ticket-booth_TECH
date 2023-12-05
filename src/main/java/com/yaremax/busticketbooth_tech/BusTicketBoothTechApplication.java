c4725271fabfd7bae8b6bf053d77953953872d03
Yarema Maksym <macsym2@gmail.com>
2023-12-05 10:30:55 +0200

    init project

---
+++ src/main/java/com/yaremax/busticketbooth_tech/BusTicketBoothTechApplication.java
@@ -0,0 +1,13 @@
+package com.yaremax.busticketbooth_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketBoothTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusTicketBoothTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaremax/busticketbooth_tech/BusTicketBoothTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaremax.busticketbooth_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBoothTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
