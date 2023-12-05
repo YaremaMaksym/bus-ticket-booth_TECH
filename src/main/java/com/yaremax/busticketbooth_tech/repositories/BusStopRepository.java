248bb9fc69431fa1b9672c179a1c2e1f60a790e2
Yarema Maksym <macsym2@gmail.com>
2023-12-05 14:39:13 +0200

    rename StopRepository -> BusStopRepository

--- src/main/java/com/yaremax/busticketbooth_tech/repositories/BusStopRepository.java
+++ src/main/java/com/yaremax/busticketbooth_tech/repositories/BusStopRepository.java
@@ -3,5 +3,5 @@ package com.yaremax.busticketbooth_tech.repositories;
 import com.yaremax.busticketbooth_tech.data.BusStop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StopRepository extends JpaRepository<BusStop, Integer> {
+public interface BusStopRepository extends JpaRepository<BusStop, Integer> {
 }
