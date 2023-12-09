aa422178c5904bec08fa98a21aa47203f47c430a
Maksym Yarema <maksym2@gmail.com>
2023-12-09 20:45:44 +0200

    one to one relation (BoardingManifest to Schedule)

--- src/main/java/com/yaremax/busticketbooth_tech/data/BoardingManifest.java
+++ src/main/java/com/yaremax/busticketbooth_tech/data/BoardingManifest.java
@@ -3,8 +3,6 @@ package com.yaremax.busticketbooth_tech.data;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @Setter
@@ -16,9 +14,7 @@ public class BoardingManifest {
     @Column(name = "manifest_id", nullable = false)
     private Integer id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @OnDelete(action = OnDeleteAction.SET_NULL)
-    @JoinColumn(name = "schedule_id")
+    @OneToOne(mappedBy = "boardingManifest")
     private Schedule schedule;
 
 }
