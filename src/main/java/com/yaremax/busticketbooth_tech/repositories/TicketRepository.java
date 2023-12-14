2db224a3188e3ff1f305d5f8f9abc468d6b3be29
Maksym Yarema <maksym2@gmail.com>
2023-12-14 14:23:44 +0200

    feat: add tickets page, and removed RefundController

--- src/main/java/com/yaremax/busticketbooth_tech/repositories/TicketRepository.java
+++ src/main/java/com/yaremax/busticketbooth_tech/repositories/TicketRepository.java
@@ -1,12 +1,36 @@
 package com.yaremax.busticketbooth_tech.repositories;
 
 import com.yaremax.busticketbooth_tech.data.Ticket;
+import com.yaremax.busticketbooth_tech.projections.TicketInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    @Query("select " +
+            "t.id as id, " +
+            "t.seatNumber as seatNumber, " +
+            "t.ticketStatus as ticketStatus, " +
+            "t.schedule as schedule, " +
+            "t.busStop as busStop " +
+            "from Ticket t " +
+            "join t.schedule s " +
+            "join t.busStop bs ")
+    List<TicketInfo> findAllTicketInfos();
+
+    @Query("select " +
+            "t.id as id, " +
+            "t.seatNumber as seatNumber, " +
+            "t.ticketStatus as ticketStatus, " +
+            "t.schedule as schedule, " +
+            "t.busStop as busStop " +
+            "from Ticket t " +
+            "join t.schedule s " +
+            "join t.busStop bs " +
+            "where t.id = :ticketId")
+    TicketInfo findByIdTicketInfo(Integer ticketId);
+
     List<Ticket> findByScheduleIdAndTicketStatus(Integer id, String status);
 
     @Query("SELECT COUNT(t) " +
--- src/main/java/com/yaremax/busticketbooth_tech/services/TicketService.java
+++ src/main/java/com/yaremax/busticketbooth_tech/services/TicketService.java
@@ -4,6 +4,7 @@ import com.yaremax.busticketbooth_tech.data.Ticket;
 import com.yaremax.busticketbooth_tech.dto.TicketDto;
 import com.yaremax.busticketbooth_tech.exception.ResourceNotFoundException;
 import com.yaremax.busticketbooth_tech.mappers.TicketMapper;
+import com.yaremax.busticketbooth_tech.projections.TicketInfo;
 import com.yaremax.busticketbooth_tech.repositories.TicketRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -41,5 +42,13 @@ public class TicketService {
 
     public Integer countTicketsForScheduleByBusSerialNumber(Integer busId) {
         return ticketRepository.countTicketsForScheduleByBusSerialNumber(busId);
+    }
+
+    public List<TicketInfo> findAllTicketInfos() {
+        return ticketRepository.findAllTicketInfos();
+    }
+
+    public TicketInfo findByIdTicketInfo(Integer ticketId) {
+        return ticketRepository.findByIdTicketInfo(ticketId);
     }
 }
